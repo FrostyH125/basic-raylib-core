c042af43f4a422b272b6b5d95ed779ff9c554b7c
ZanderHyDe <zandersylvester@gmail.com>
2026-03-03 07:04:42 -1000

    added draw_pro to sprite

--- src/graphics/sprite.rs
+++ src/graphics/sprite.rs
@@ -14,4 +14,15 @@ impl Sprite {
     pub fn draw(&self, d: &mut RaylibDrawHandle, pos: Vector2, texture: &Texture2D) {
         d.draw_texture_rec(texture, self.src_rect, pos, Color::WHITE);
     }
+
+    pub fn draw_pro(
+        &self,
+        d: &mut RaylibDrawHandle,
+        dest_rect: Rectangle,
+        origin: Vector2,
+        rotation: f32,
+        texture: &Texture2D,
+    ) {
+        d.draw_texture_pro(texture, self.src_rect, dest_rect, origin, rotation, Color::WHITE);
+    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,3 @@
 pub mod graphics;
 pub mod system;
+pub mod utils;
