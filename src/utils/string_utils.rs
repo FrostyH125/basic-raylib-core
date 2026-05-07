8d21b275ba72c37d67d6d33ce4c369167d812a78
Zander Sylvester <zandersylvester@gmail.com>
2026-05-07 13:19:57 -1000

    added a string util for drawing a string centered on a point

--- src/utils/string_utils.rs
+++ src/utils/string_utils.rs
@@ -1,4 +1,4 @@
-use raylib::text::{Font, RaylibFont};
+use raylib::{color::Color, drawing::{RaylibDraw, RaylibDrawHandle}, math::Vector2, text::{Font, RaylibFont}};
 
 pub fn wrap_string(string: &str, max_length_in_pixels: f32, font: &Font, font_size: f32, spacing: f32) -> String {
     let words: Vec<&str> = string.split(" ").collect();
@@ -18,16 +18,16 @@ pub fn wrap_string(string: &str, max_length_in_pixels: f32, font: &Font, font_si
 
         if line_size.x >= max_length_in_pixels {
             current_line.truncate(original_len);
-            
+
             if !current_line.is_empty() {
                 final_string.push_str(&current_line);
                 final_string.push('\n');
             }
-            
+
             current_line.clear();
             current_line.push_str(word);
         }
-        
+
         //only reasonably should happen if the word ends with \n
         if current_line.contains('\n') {
             final_string.push_str(&current_line);
@@ -40,4 +40,12 @@ pub fn wrap_string(string: &str, max_length_in_pixels: f32, font: &Font, font_si
     }
 
     final_string
+}
+
+pub fn draw_string_centered_on_pos(d: &mut RaylibDrawHandle, pos: Vector2, string: &str, font: &Font, font_size: f32, spacing: f32, color: Color) {
+    let size_of_string = font.measure_text(string, font_size, spacing);
+
+    let draw_pos = pos - (size_of_string / 2.0);
+
+    d.draw_text_ex(font, string, draw_pos, font_size, spacing, color);
 }
