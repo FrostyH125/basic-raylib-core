a00d65744b2c8f87e3e17019df80cd4037af7051
Zander-Laptop <zandersylvester@gmail.com>
2026-05-21 12:43:20 -1000

    changed sprite particle system to use packed array instead of naive approach

--- src/system/sprite_particle_system.rs
+++ src/system/sprite_particle_system.rs
@@ -5,11 +5,26 @@ use crate::system::sprite_particle::SpriteParticle;
 
 pub struct SpriteParticleSystem {
     particles: Vec<SpriteParticle>,
+    max_particles: usize,
+    n_particles: usize,
 }
 
 impl SpriteParticleSystem {
-    pub fn new() -> Self {
-        SpriteParticleSystem { particles: Vec::new() }
+    pub fn new(max_particles: usize) -> Self {
+
+        let mut particles: Vec<SpriteParticle> = Vec::new();
+        
+        for _ in 0..max_particles {
+            //initialize vector with dummy particles
+            let p = SpriteParticle::new_default();
+            particles.push(p);
+        }
+        
+        SpriteParticleSystem {
+            particles,
+            n_particles: 0,
+            max_particles,
+        }
     }
 
     pub fn emit(
@@ -20,6 +35,10 @@ impl SpriteParticleSystem {
         acceleration: Vector2,
         lifetime: f32,
     ) {
+        if self.n_particles >= self.max_particles {
+            return;
+        }
+
         let sprite_half_width = sprite.src_rect.width / 2.0;
         let sprite_half_height = sprite.src_rect.height / 2.0;
 
@@ -38,20 +57,44 @@ impl SpriteParticleSystem {
             lifetime,
         };
 
-        self.particles.push(particle);
+        self.particles[self.n_particles] = particle;
+        self.n_particles += 1;
     }
 
     pub fn update(&mut self, dt: f32) {
-        for i in (0..self.particles.len()).rev() {
+        let mut i = 0;
+
+        while i < self.n_particles {
             let particle = &mut self.particles[i];
 
-            particle.position += particle.velocity * dt;
-            particle.velocity += particle.acceleration * dt;
             particle.lifetime -= dt;
 
             if particle.lifetime <= 0.0 {
-                self.particles.remove(i);
+                // move this particle to the absolute edge of the array
+                // it doesnt matter what was there before, as it wasnt active,
+                // and order doesnt matter
+                // this also moves the last active particle into the inner array somewhere
+                // which keeps it packed
+                // 
+                // this is basic but just so i can keep things straight
+                // if theres 10 particles, we're updating 0..=9
+                // this means that swapping n_particles - 1 will swap with the idx 9 (the last one)
+                // then decrementing n_particles means we now update 0..=8, which is great
+                // because idx 9 is where it stops being active particles
+                // (i am extremely dense and need this reminder)
+                self.particles.swap(self.n_particles - 1, i);
+
+                // THEN decrement n_particles as to not have it update the newly
+                // expired particle
+                self.n_particles -= 1;
+
+                continue;
             }
+
+            particle.position += particle.velocity * dt;
+            particle.velocity += particle.acceleration * dt;
+
+            i += 1;
         }
     }
 
@@ -60,4 +103,22 @@ impl SpriteParticleSystem {
             particle.sprite.draw(d, particle.position, texture);
         }
     }
+}
+
+#[test]
+fn test_ps_emit_and_expire() {
+    let mut ps = SpriteParticleSystem::new(1000);
+    static NOTHING_SPRITE: Sprite = Sprite::new(0.0, 0.0, 0.0, 0.0);
+    
+    for _ in 0..=1500 {
+        ps.emit(&NOTHING_SPRITE, Vector2::zero(), Vector2::zero(), Vector2::zero(), 1.0);
+    }
+
+    assert!(ps.n_particles == 1000);
+
+    let dt = 1.1;
+
+    ps.update(dt);
+
+    assert!(ps.n_particles == 0);
 }
