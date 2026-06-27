4793133fd701c4035e41918b276527167368ce2e
Zander-Laptop <zandersylvester@gmail.com>
2026-06-26 14:09:39 -1000

    added rotation to sprite particles

--- src/graphics/sprite_particle.rs
+++ src/graphics/sprite_particle.rs
@@ -6,11 +6,12 @@ static DUMMY_SPRITE: Sprite = Sprite::new(0, 0, 0, 0);
 pub struct SpriteParticle {
     pub sprite: &'static Sprite,
     pub position: Vector2,
+    pub origin: Vector2,
     pub velocity: Vector2,
     pub acceleration: Vector2,
+    pub rotation_speed: f32,
+    pub current_rotation: f32,
     pub lifetime: f32,
-    // rotation
-    // origin
 }
 
 impl SpriteParticle {
@@ -18,9 +19,12 @@ impl SpriteParticle {
         SpriteParticle {
             sprite: &DUMMY_SPRITE,
             position: Default::default(),
+            origin: Default::default(),
             velocity: Default::default(),
-            acceleration: Default::default(),
             lifetime: Default::default(),
+            rotation_speed: Default::default(),
+            current_rotation: Default::default(),
+            acceleration: Default::default(),
         }
     }
 }
--- src/system/sprite_particle_system.rs
+++ src/system/sprite_particle_system.rs
@@ -1,5 +1,9 @@
 use crate::graphics::sprite::Sprite;
-use raylib::{math::Vector2, prelude::RaylibDrawHandle, texture::Texture2D};
+use raylib::{
+    math::{Rectangle, Vector2},
+    prelude::RaylibDrawHandle,
+    texture::Texture2D,
+};
 
 use crate::graphics::sprite_particle::SpriteParticle;
 
@@ -11,20 +15,15 @@ pub struct SpriteParticleSystem {
 
 impl SpriteParticleSystem {
     pub fn new(max_particles: usize) -> Self {
-
         let mut particles: Vec<SpriteParticle> = Vec::new();
-        
+
         for _ in 0..max_particles {
             //initialize vector with dummy particles
             let p = SpriteParticle::new_default();
             particles.push(p);
         }
-        
-        SpriteParticleSystem {
-            particles,
-            n_particles: 0,
-            max_particles,
-        }
+
+        SpriteParticleSystem { particles, n_particles: 0, max_particles }
     }
 
     pub fn emit(
@@ -49,20 +48,64 @@ impl SpriteParticleSystem {
         // other side
         let real_emit_pos = Vector2::new(position.x - sprite_half_width, position.y - sprite_half_height);
 
-        let particle = SpriteParticle {
-            sprite,
-            position: real_emit_pos,
-            velocity,
-            acceleration,
-            lifetime,
-        };
+        self.particles[self.n_particles].sprite = sprite;
+        self.particles[self.n_particles].position = real_emit_pos;
+        self.particles[self.n_particles].origin = Vector2::zero();
+        self.particles[self.n_particles].velocity = velocity;
+        self.particles[self.n_particles].acceleration = acceleration;
+        self.particles[self.n_particles].rotation_speed = 0.0;
+        self.particles[self.n_particles].current_rotation = 0.0;
+        self.particles[self.n_particles].lifetime = lifetime;
 
-        self.particles[self.n_particles] = particle;
         self.n_particles += 1;
     }
 
-    // emit_ex with rotation 
-    // the origin would automatically have to be placed as the center
+    /// preserve_original_pos will prevent the object from shifting when the origin is set.
+    /// use it if you need things to be at pixel perfect, 
+    /// otherwise if you'd rather the object be centered
+    /// like is automatic in the regular emit(), then leave it false
+    pub fn emit_ex(
+        &mut self,
+        sprite: &'static Sprite,
+        position: Vector2,
+        velocity: Vector2,
+        acceleration: Vector2,
+        rotation_speed: f32,
+        starting_rotation: f32,
+        lifetime: f32,
+        preserve_original_pos: bool,
+    ) {
+        if self.n_particles >= self.max_particles {
+            return;
+        }
+
+        let sprite_half_width = sprite.src_rect.width / 2.0;
+        let sprite_half_height = sprite.src_rect.height / 2.0;
+        let origin = Vector2::new(sprite_half_width, sprite_half_height);
+
+        // remove the shift done by changing the origin
+        let real_emit_pos = match preserve_original_pos {
+            true => {
+                position
+                    + Vector2 {
+                        x: sprite_half_width,
+                        y: sprite_half_height,
+                    }
+            }
+            false => position,
+        };
+
+        self.particles[self.n_particles].sprite = sprite;
+        self.particles[self.n_particles].position = real_emit_pos;
+        self.particles[self.n_particles].origin = origin;
+        self.particles[self.n_particles].velocity = velocity;
+        self.particles[self.n_particles].acceleration = acceleration;
+        self.particles[self.n_particles].rotation_speed = rotation_speed;
+        self.particles[self.n_particles].current_rotation = starting_rotation;
+        self.particles[self.n_particles].lifetime = lifetime;
+
+        self.n_particles += 1;
+    }
 
     pub fn update(&mut self, dt: f32) {
         let mut i = 0;
@@ -78,7 +121,7 @@ impl SpriteParticleSystem {
                 // and order doesnt matter
                 // this also moves the last active particle into the inner array somewhere
                 // which keeps it packed
-                // 
+                //
                 // this is basic but just so i can keep things straight
                 // if theres 10 particles, we're updating 0..=9
                 // this means that swapping n_particles - 1 will swap with the idx 9 (the last one)
@@ -96,6 +139,7 @@ impl SpriteParticleSystem {
 
             particle.position += particle.velocity * dt;
             particle.velocity += particle.acceleration * dt;
+            particle.current_rotation += particle.rotation_speed * dt;
 
             i += 1;
         }
@@ -104,8 +148,19 @@ impl SpriteParticleSystem {
     pub fn draw(&self, d: &mut RaylibDrawHandle, texture: &Texture2D) {
         for i in 0..self.n_particles {
             let particle = &self.particles[i];
-            
-            particle.sprite.draw(d, particle.position, texture);
+
+            particle.sprite.draw_pro(
+                d,
+                Rectangle::new(
+                    particle.position.x,
+                    particle.position.y,
+                    particle.sprite.src_rect.width,
+                    particle.sprite.src_rect.height,
+                ),
+                particle.origin,
+                particle.current_rotation,
+                texture,
+            );
         }
     }
 }
@@ -114,7 +169,7 @@ impl SpriteParticleSystem {
 fn test_ps_emit_and_expire() {
     let mut ps = SpriteParticleSystem::new(1000);
     static NOTHING_SPRITE: Sprite = Sprite::new(0, 0, 0, 0);
-    
+
     for _ in 0..=1500 {
         ps.emit(&NOTHING_SPRITE, Vector2::zero(), Vector2::zero(), Vector2::zero(), 1.0);
     }
