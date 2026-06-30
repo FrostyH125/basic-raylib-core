9dcf674870f99f899bb361dd4d8cf1fc1e382056
Zander Sylvester <zandersylvester@gmail.com>
2026-06-30 08:09:09 -1000

    added center rect

--- src/utils/math_utils.rs
+++ src/utils/math_utils.rs
@@ -1,6 +1,7 @@
 use rand::{RngExt, rngs::ThreadRng};
-use raylib::math::Vector2;
+use raylib::math::{Rectangle, Vector2};
 
+#[inline]
 pub fn smooth_lerp(start: f32, end: f32, current_time: f32, total_duration: f32) -> f32 {
     let progress = current_time / total_duration;
     let t = progress.clamp(0.0, 1.0);
@@ -10,9 +11,14 @@ pub fn smooth_lerp(start: f32, end: f32, current_time: f32, total_duration: f32)
 
 /// returns a Vector2 with x & y: -1.0..=1.0
 /// takes a threadrng as to avoid allocating one through repeated calls
+#[inline]
 pub fn random_dir(rng: &mut ThreadRng) -> Vector2 {
     let rand_x = rng.random_range(-1.0..=1.0);
     let rand_y = rng.random_range(-1.0..=1.0);
     
     return Vector2 { x: rand_x, y: rand_y };
+}
+ #[inline]
+pub fn center_of_rect(rect: Rectangle) -> Vector2 {
+    return Vector2::new(rect.x + (rect.width / 2.0), rect.y + (rect.height / 2.0));
 }
